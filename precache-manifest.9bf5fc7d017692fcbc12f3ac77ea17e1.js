0ba3837bee40b272e149907744d4c9d38271ccd7
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-10-11 21:13:40 -0500

    Updates

---
+++ precache-manifest.9bf5fc7d017692fcbc12f3ac77ea17e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc596dbdd657b4c6c7eefe2e5072ff8",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "ab15893427e50b1a690b",
+    "url": "/react-portfolio/static/css/main.256d3fd7.chunk.css"
+  },
+  {
+    "revision": "2cf709f5f5f1cc32bb50",
+    "url": "/react-portfolio/static/js/2.14a40f06.chunk.js"
+  },
+  {
+    "revision": "2927c17f155caba9c734acdc50e6f829",
+    "url": "/react-portfolio/static/js/2.14a40f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab15893427e50b1a690b",
+    "url": "/react-portfolio/static/js/main.b62fe80b.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
