864698b54343ae373012d65c5263749d7473ce86
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-10-17 11:54:09 -0500

    switched to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import About from './components/About';
 import Projects from './components/Projects/Projects';
 import Footer from './components/Footer';
@@ -12,13 +12,13 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div className='App'>
-      <Router basename="/react-portfolio/">
+      <Router basename="/">
         <Nav />
         <Switch>
-          <Route path='/react-portfolio/' exact component={About}/>
-          <Route path='/react-portfolio/projects' component={Projects} />
-          <Route path='/react-portfolio/resume' component={Resume} />
-          <Route path='/react-portfolio/contact' component={Contact} />
+          <Route path='/home' exact component={About}/>
+          <Route path='/projects' component={Projects} />
+          <Route path='/resume' component={Resume} />
+          <Route path='/contact' component={Contact} />
         </Switch>
       </Router>
       <main></main>
